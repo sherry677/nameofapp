4702b59b0f79ae890ee98e6ec973c098b62c4c5d
Yinnan <yinnanliu@hotmail.com>
2016-12-12 14:42:13 -0600

    4.2 before apply the ERB tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209214805) do
+ActiveRecord::Schema.define(version: 20161212200908) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20161209214805) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
+    t.string   "price"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,6 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   Mayor.create(name: 'Emanuel', city: cities.firstProduct.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+Product.create(:name => "English mastiff", :description => "he English mastiff is enormous. Growing to a height of 30 inches, these dogs can weigh as much as 250 pounds. As a Great Dane holds the record for tallest dog, an English mastiff holds the record for heaviest!", :text => "The biggest weight for a dog ever recorded was an English mastiff named Aicama Zorba, who weighed in at 343 pounds.")
